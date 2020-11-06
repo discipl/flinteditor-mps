6ca9adafb8eee4e26dc3138ef0c471ea4bc09834
blackstardlb <brisondl@gmail.com>
2020-11-06 11:20:46 +0100

    Switched to using env variables for tests

--- code/java/FlintParser/src/test/kotlin/org/discipl/flint/flintfiller/FlintFillerTest.kt
+++ code/java/FlintParser/src/test/kotlin/org/discipl/flint/flintfiller/FlintFillerTest.kt
@@ -7,18 +7,14 @@ import org.junit.jupiter.api.Test
 import java.nio.file.Path
 
 internal class FlintFillerTest {
-    private val projectPath = Path.of("../../..").toRealPath()
     private val filler: FlintFiller = FlintFiller(
-        projectPath.resolve("static-resources/generated/flintfiller").toString(),
-        projectPath.resolve("build/temp").toString()
+        System.getenv("FLINT_FILLER_DIR"),
+        System.getenv("FLINT_FILLER_OUTPUT_DIR")
     )
 
     @Test
     fun run() {
-        val flintModel = filler.run(
-            projectPath.resolve("code/java/FlintParser/src/test/resources/flint-filler-test-file.xml")
-                .toString()
-        )
+        val flintModel = filler.run(System.getenv("FLINT_FILLER_TEST_FILE"))
         println(flintModel.substring(0..400))
         assertThat(flintModel, `is`(notNullValue()))
     }
